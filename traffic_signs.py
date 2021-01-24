2b2238723143d9283c40ddc8b6776ce3b5b1663e
Anup Kumar Mishra <anupkumar08@gmail.com>
2021-01-25 00:17:52 +0530

    Add files via upload

---
+++ traffic_signs.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from PIL import Image
+import os
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from sklearn.metrics import accuracy_score
+
+# Step 1: Explore the dataset
+
+data = []
+labels = []
+classes = 43
+cur_path = os.getcwd()
+
+# Retrieving the images and their labels
+for i in range(classes):
+    path = os.path.join(cur_path, 'train', str(i))
+    images = os.listdir(path)
+
+    for a in images:
+        try:
+            image = Image.open(path + '\\' + a)
+            image = image.resize((30, 30))
+            image = np.array(image)
+            # sim = Image.fromarray(image)
+            data.append(image)
+            labels.append(i)
+        except:
+            print("Error loading image")
+
+# Converting lists into numpy arrays
+data = np.array(data)
+labels = np.array(labels)
+
+# check shape of data and labels
+print(data.shape, labels.shape)
+
+# Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# Converting the labels into one hot encoding
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# Step 2: Build a CNN model
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.25))
+model.add(Dense(43, activation='softmax'))
+
+# compilation of model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
+
+# Steps 3: Train and validate the model
+epochs = 15
+history = model.fit(X_train, y_train, batch_size=64, epochs=epochs, validation_data=(X_test, y_test))
+
+# plotting graphs for accuracy
+plt.figure(0)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+plt.figure(1)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Loss')
+plt.xlabel('Epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+
+# Step 4: Test our model with test dataset
+
+# testing accuracy on test dataset
+
+y_test = pd.read_csv("Test.csv")
+
+labels = y_test["ClassId"].values
+imgs = y_test["Path"].values
+
+data = []
+
+for img in imgs:
+    image = Image.open(img)
+    image = image.resize((30, 30))
+    data.append(np.array(image))
+
+X_test = np.array(data)
+
+pred = model.predict_classes(X_test)
+
+# Accuracy with the test data
+print(accuracy_score(labels, pred))
+
+model.save("traffic_classifier.h5")
